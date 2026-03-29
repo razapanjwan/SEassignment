9693b356db0984ce150e219f2644e0e2353aba55
razapanjwan <razapanjwani16986@gmail.com>
2026-03-29 18:47:35 +0500

    .

--- obj/Debug/net8.0-windows/QueueCalc.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/QueueCalc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueueCalc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bcff6737d1d96b7f4a4b9ff16144357f623a8b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueueCalc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueueCalc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
